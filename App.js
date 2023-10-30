8b84870eb6589910bc0eb576e6a00c057658c9c7
AlxProg25 <aleal-15@gmail.com>
2023-10-29 21:41:49 -0400

    CodigoAutent

--- App.js
+++ App.js
@@ -1,12 +1,112 @@
+//Importaciones
+import React, { useState }  from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, TouchableOpacity, TextInput, Button, Image } from 'react-native';
+import { BlurView } from 'expo-blur';
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
+import { firebaseConfig } from './firebase-config';
+import { initializeApp } from 'firebase/app';
+
+
+//IMAGEN DE FONDO
+const uri= "https://i.pinimg.com/236x/81/72/f3/8172f32cc85e02fe8c7cec5f5fd176fb.jpg";
+
+//Funcion para pantalla Inicio
+function PantallaInicio(){
+  return (
+    <View style={{flex:1, alignItems:"center", justifyContent:"center"}}>
+      <Text>Pantalla Inciio</Text>
+    </View>
+  );
+}
+
+//Funcion para pantalla Login
+function PantallaLogin(){
+  const [email, setEmail] = React.useState('')
+  const [password, setPassword]= React.useState('')
+  const app = initializeApp(firebaseConfig);
+  const auth = getAuth(app);
+
+//2 funciones para crear cuenta y iniciar sesion
+const CrearCuenta = () =>{
+  createUserWithEmailAndPassword(auth,email,password)
+  .then((userCredential)=>{
+    console.log("cuenta creada correctamente")
+    const user=userCredential.user;
+    console.log(user);
+  })
+  .catch(error=>{
+    console.log(error)
+    Alert.alert(error.mesage)
+  })
+  
+}
+
+const InicioSesion = () =>{
+  signInWithEmailAndPassword(auth, email,password)
+  .then((userCredential)=>{
+    console.log("Inicio de sesion correcto... bienvenido.")
+    const user=userCredential.user;
+    console.log(user)
+  })
+  .catch(error=>{
+    console.log(error)
+  })
+}
+
+
+  return (
+    //componentes que interactua el usuario
+    <View style={styles.container}>
+      <Image 
+        source={{uri}}
+        style={[styles.imagen, StyleSheet.absoluteFill]}
+      />
+      <ScrollView
+      contentContainerStyle={{
+        width:"100%",
+        height: "100%",
+        alignItems: "center",
+        justifyContent: "center",
+        flex: 1,
+      }}>
+        
+        <BlurView intensity={80}>
+          <View style={styles.login}>
+            <View>
+
+              <Text style={{fontSize:16, fontWeight:"300", color:"#fff"}}>
+                Correo
+              </Text>
+              <TextInput onChangeText={(text)=>setEmail(text)} style={styles.input} placeholder="correo@ejemplo.com"/>
+
+              <Text style={{fontSize:16, fontWeight:"300", color:"#fff"}}>
+                Contraseña
+              </Text>
+              <TextInput onChangeText={(text)=>setPassword(text)} style={styles.input} placeholder="contraseña" secureTextEntry={true}/>
+              
+            </View>
+            <View>
+              <TouchableOpacity onPress={InicioSesion} style={styles.boton}>
+                <Text style={{fontSize:16, fontWeight:"300", color: "#fff"}}>Iniciar sesion</Text>
+              </TouchableOpacity>
+              <TouchableOpacity onPress={CrearCuenta} style={styles.boton}>
+                <Text style={{fontSize:16, fontWeight:"300", color: "#fff"}}>Registrarse</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </BlurView>
+
+      </ScrollView>
+    </View>
+  );
+}
+
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PantallaLogin/>
   );
 }
 
@@ -17,4 +117,29 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  imagen:{
+    //ESTILOS
+  },
+  login:{
+    //Estilos login
+  },
+  input:{
+    width:200,
+    height:50,
+    borderColor:'#fff',
+    borderRadius:20,
+    padding:15,
+    backgroundColor:'green',
+
+  },
+  boton:{
+    width:200,
+    height:50,
+    borderRadius:20,
+    backgroundColor:'green',
+    alignItems:'center',
+    justifyContent:'center',
+    borderColor:'#fff'
+
+  }
 });
---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+import { initializeApp } from 'firebase/app';
+export const firebaseConfig = {
+  apiKey: "AIzaSyBQQ8ZxSqxL1Y7_NBjwRC6cgrQ1LhmxT2o",
+  authDomain: "autentemail.firebaseapp.com",
+  projectId: "autentemail",
+  storageBucket: "autentemail.appspot.com",
+  messagingSenderId: "53628442605",
+  appId: "1:53628442605:web:35041f798ca4d6708879e4"
+};
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
